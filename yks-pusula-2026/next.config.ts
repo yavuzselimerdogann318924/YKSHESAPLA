20390375217be93651fe58b2a9f496bb42fc90ee
Yavuz selim Erdoğan <116522873+yavuzselimerdogann318924@users.noreply.github.com>
2026-08-18 19:54:15 +0300

    Remove trailingSlash option from next.config.ts

--- yks-pusula-2026/next.config.ts
+++ yks-pusula-2026/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  trailingSlash: true,
   images: { unoptimized: true },
   typescript: { ignoreBuildErrors: true },
 };
